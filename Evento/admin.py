1088e6c4668c25a3d42caf16de5a525b733bfd9a
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-15 11:09:53 -0300

    Deploy heroku

---
+++ Evento/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import Evento
+@admin.register(Evento)
+class EventoAdmin(admin.ModelAdmin):
+    list_display=('tipo', 'data_inicio', 'data_fim', 'descricao', 'importante')
---
+++ Evento/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Evento(models.Model):
+    tipo = models.CharField(max_length=250)
+    data_inicio = models.DateField()
+    data_fim = models.DateField(blank=True, null=True)
+    descricao = models.CharField(max_length=250)
+    importante = models.BooleanField()
+
+    
+
+    def __str__(self):
+        return str(self.id)
