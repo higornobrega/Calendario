1155f4d3e11ebd0947e15af9c3bbf23850c8f590
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-15 17:22:31 -0300

    modificando tipo

--- Evento/models.py
+++ Evento/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Evento(models.Model):
-    tipo = models.CharField(max_length=250)
+    SHIRT_SIZES = (
+        ('Feriados Nacionais e Estaduais', 'Feriados Nacionais e Estaduais'),
+        ('Feriados Municipais', 'Feriados Municipais'),
+        ('Datas Comemorativas', 'Datas Comemorativas'),
+        ('Datas da Instituinção', 'Datas da Instituinção'),
+    )
+    tipo = models.CharField(max_length=250, choices=SHIRT_SIZES)
     data_inicio = models.DateField()
     data_fim = models.DateField(blank=True, null=True)
     descricao = models.CharField(max_length=250)
